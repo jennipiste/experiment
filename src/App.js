3142ecf2b48a65aabe95ee5fe978bb5306ddd743
Jenni Pajukoski <jenni@giosg.com>
2018-04-26 13:48:34 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Chat multitasking experiment</h1>
+        </header>
+        <p className="App-intro">
+          Let's test how many chats you can handle
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
