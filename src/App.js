11a53ebeae410a20671ea74c595312622155128e
Jenni Pajukoski <jenni@giosg.com>
2018-04-28 15:56:09 +0300

    Create participant before starting experiment

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react'; // eslint-disable-line no-unused-vars
 import { Route } from 'react-router-dom';
+import axios from'axios';
+import axiosDefaults from 'axios/lib/defaults';
 import ChatDialogGrid from './ChatDialogGrid';
 import ChatDialogList from './ChatDialogList';
 import './App.css';
+
+axiosDefaults.baseURL = 'http://localhost:8000';
 
 class App extends Component {
   constructor(props) {
@@ -10,6 +14,8 @@ class App extends Component {
 
     this.state = {
       dialogs: [],
+      participantName: "",
+      activeParticipant: null,
     };
 
   }
@@ -66,20 +72,47 @@ class App extends Component {
         name: "Chat Dialog 3",
         id: 3,
       },
-    ]
+    ];
     this.setState({dialogs: dialogs});
+  }
+
+  onParticipantNameChange = (event) => {
+    this.setState({participantName: event.target.value});
+  }
+
+  createParticipant = (event) => {
+    event.preventDefault();
+    axios.post('api/participants', {name: this.state.participantName})
+      .then(response => {
+        this.setState({
+          activeParticipant: response.data,
+        });
+      });
   }
 
   render() {
     return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">Chat multitasking experiment</h1>
-        <p className="App-intro">Let's test how many chats you can handle</p>
-      </header>
-      <Route path="/exp1" render={() => <ChatDialogGrid dialogs={this.state.dialogs}/>} />
-      <Route path="/exp2" render={() => <ChatDialogList dialogs={this.state.dialogs}/>}/>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Chat multitasking experiment</h1>
+          <p className="App-intro">{this.state.activeParticipant && "Welcome " + this.state.activeParticipant.name + "! "}Let's test how many chats you can handle</p>
+        </header>
+        {this.state.activeParticipant ? (
+          <div>
+            <Route path="/exp1" render={() => <ChatDialogGrid dialogs={this.state.dialogs}/>} />
+            <Route path="/exp2" render={() => <ChatDialogList dialogs={this.state.dialogs}/>}/>
+          </div>
+        ) : (
+          <div className="CreateParticipant">
+            <form onSubmit={this.createParticipant}>
+              <label>Name:
+                <input type="text" value={this.state.participantName} onChange={this.onParticipantNameChange} />
+              </label>
+              <input type="submit" value="Start" />
+            </form>
+          </div>
+        )}
+      </div>
     );
   }
 }
