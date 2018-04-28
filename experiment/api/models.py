cabce9f74c009a878430c5a01471d08147541a02
Jenni Pajukoski <jenni@giosg.com>
2018-04-28 14:19:47 +0300

    Created Participant, ChatMessage and ChatDialog models

--- experiment/api/models.py
+++ experiment/api/models.py
@@ -1,6 +1,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+
+class Participant(models.Model):
+    created_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=400)
+
+
+class ChatDialog(models.Model):
+    created_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=400)
+    participant = models.ForeignKey(Participant)
+
+
+class ChatMessage(models.Model):
+    MESSAGE_TYPES = (
+        (1, "incoming"),
+        (2, "outgoing"),
+    )
+    created_at = models.DateTimeField(auto_now=True)
+    message = models.TextField()
+    sender = models.ForeignKey(Participant, null=True)
+    type = models.IntegerField(choices=MESSAGE_TYPES)
+    chat_dialog = models.ForeignKey(ChatDialog)
