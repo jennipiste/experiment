b442ecd378b01ccf2f0ba72369f24c9522f230eb
Jenni Pajukoski <jenni@giosg.com>
2018-04-29 17:56:29 +0300

    Fixed auto_now_add, name is unique, added __str__ methods

--- experiment/api/models.py
+++ experiment/api/models.py
@@ -4,14 +4,20 @@ from django.db import models
 
 
 class Participant(models.Model):
-    created_at = models.DateTimeField(auto_now=True)
-    name = models.CharField(max_length=400)
+    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=400, unique=True)
+
+    def __str__(self):
+        return u"name: {0}, id: {1}".format(self.name, self.id)
 
 
 class ChatDialog(models.Model):
-    created_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=400)
     participant = models.ForeignKey(Participant)
+
+    def __str__(self):
+        return u"name: {0}, id: {1}".format(self.name, self.id)
 
 
 class ChatMessage(models.Model):
@@ -19,8 +25,11 @@ class ChatMessage(models.Model):
         (1, "incoming"),
         (2, "outgoing"),
     )
-    created_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     message = models.TextField()
     sender = models.ForeignKey(Participant, null=True)
     type = models.IntegerField(choices=MESSAGE_TYPES)
     chat_dialog = models.ForeignKey(ChatDialog)
+
+    def __str__(self):
+        return u"message: {0}, type: {}, sender: {}".format(self.message, self.type, self.sender)
