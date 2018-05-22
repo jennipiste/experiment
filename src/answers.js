bdb97e137acb8e23a784ed9dcea199acc1c1b022
Jenni Pajukoski <jenni@giosg.com>
2018-05-22 16:51:55 +0300

    Seprated questions and answers and added wait times for each subject

---
+++ src/answers.js
@@ -0,0 +1,128 @@
+const answers = {
+  'Astianpesukone': [
+    "596 mm",
+    "Vesihanan avaaminen",
+    "3",
+    "70 astetta",
+    "Koneeseen jää vettä",
+    "7",
+    "Parempi hygieniataso",
+
+  ],
+  'Cheerleadingin kilpailusäännöt': [
+    "14x16 metriä",
+    "6",
+    "10",
+    "Ei",
+    "2:30",
+    "24",
+    "3",
+    "On",
+    "2",
+  ],
+  'Jääkaappi': [
+    "3 cm",
+    "Painaa lämpötilavalitsinta 1",
+    "-24 astetta",
+    "Painamalla lämpötilavalitsinta 1 niin monta kertaa kunnes näyttö super 2 syttyy",
+    "Demotoiminto on kytketty toimintaan",
+    "0207 510 700",
+    "On",
+  ],
+  'Kouluratsastuksen kilpailusäännöt': [
+    "2",
+    "Knalli tai silinteri",
+    "Ei",
+    "Painorajoitusta ei ole",
+    "2",
+    "Voltti, serpentiini ja volttikahdeksikko",
+
+  ],
+  'Langattomat Bluetooth-kuulokkeet': [
+    "Kuulokkeet, USB-latauskaapeli ja pikaopas",
+    "10 metriä",
+    "2 tuntia",
+    "0000",
+    "Painamalla + näppäintä pitkään",
+    "Painamalla + ja - näppäimiä",
+    "Punainen valo on sammunut",
+    "Laite ei ole toiminta-alueella",
+
+  ],
+  'Liesi': [
+    "Ei",
+    "190-210 astetta",
+    "4",
+    "650 mm",
+    "Ei",
+
+  ],
+  'Liesituuletin': [
+    "2",
+    "Painamalla käynnistys- ja + painikkeita samanaikaisesti 3 sekunnin ajan",
+    "Painamalla - kunnes merkki ilmestyy näyttöön",
+    "Lasinpesuainetta",
+    "2 kuukauden välein",
+    "HSGSB/C/UB-30-230-E14",
+
+  ],
+  'Miekkailu': [
+    "Kalpa",
+    "770 grammaa",
+    "Koko keho",
+    "Koko vartalo vyötäröstä ylöspäin",
+    "Maskia, miekkailutakkia ja -housuja, miekkailuhanskaa ja vartalojohtoa",
+    "5",
+    "3",
+    "Allez! (Alkakaa!)",
+  ],
+  'Mikroaaltouuni': [
+    "Mikroaallot eivät läpäise metallia (ja ruoka jää kylmäksi)",
+    "Sähkökatko",
+    "800 W",
+    "Nesteitä",
+    "382 mm",
+
+  ],
+  'Pöytätenniksen säännöt': [
+    "Pituus 274 cm ja leveys 152,5 cm",
+    "16 cm",
+    "Kyllä",
+    "11",
+    "Paidan, shortsien ja hameen väri täytyy olla selvästi eri kuin käytetyn pallon väri",
+    "Et",
+
+  ],
+  'Pyykinpesukone': [
+    "7 kg",
+    "Luukku on auki tai se on suljettu virheellisesti",
+    "Ei",
+    "Hienopesu",
+    "Ei",
+
+  ],
+  // 'Samsung Galaxy S9': [
+  //   "Palvelu jonka avulla puhelinta voi käyttää tietokoneen tapaan yhdistämällä puhelimen ulkoiseen näyttöön",
+
+  // ],
+  // 'Stereot': [],
+  'Suunnistuksen lajisäännöt': [
+    "Kilpailukarttaa, rastinmääritteitä ja kompassia",
+    "Yhtä",
+    "Maalin kautta (vaikuttamatta kilpailun kulkuun)",
+    "Kyllä",
+  ],
+  'Televisio': [
+    "10 Mbps",
+    "Asetukset -> Järjestelmä -> Asiantuntijan asetukset > Suorita Smart Hub automaattisesti",
+
+  ],
+  'Tenniksen kilpailumääräykset': [
+    "6",
+    "13-24",
+    "60 min",
+    "Pelipaita tennikseen tarkoitettu tekninen paita tai kauluspaita"
+  ],
+};
+
+export default answers;
